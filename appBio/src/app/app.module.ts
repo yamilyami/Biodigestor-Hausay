cc1254e2721cff7591905131f3f75af28996db72
Leandro Nallib Asis <leanx777@gmail.com>
2024-10-28 23:21:03 +0100

    para arreglar

---
+++ appBio/src/app/app.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; // Asegúrate de importar esto
+//import { AppComponent } from './app.component';
+import { ForumComponent } from './forum/forum.component';
+
+
