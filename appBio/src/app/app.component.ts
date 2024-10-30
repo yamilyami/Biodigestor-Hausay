2eb360f9a81721c9c319ae8ab4a3d1b8a7c70367
Paz <109832507+Paz-Santangelo@users.noreply.github.com>
2024-10-30 14:48:31 -0300

    Resolviendo conflictos de app.component.ts

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -1,37 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
- 69-tk-revisar-el-estilo-visual
 import { Registro01Component } from './registro01/registro01/registro01.component';
-
- develop
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { RegistroComponent } from "./registro/registro.component";
 import { AceptarTerminosComponent } from "./aceptar-terminos/aceptar-terminos.component";
 import { RestablecerContrasenaComponent } from './restablecer-contrasena/restablecer-contrasena.component';
- 69-tk-revisar-el-estilo-visual
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashBoardComponent } from './Dash/dash-board/dash-board.component';
-
 import { QuienessomosComponent } from "./QuienesSomos/quienessomos/quienessomos.component";
 import { LoginComponent } from "./login/login.component";
 import { CommonModule } from '@angular/common'; // Importar CommonModule
- develop
 
 @Component({
   selector: 'app-root',
   standalone: true,
- 69-tk-revisar-el-estilo-visual
   imports: [RouterOutlet, NadvarComponent, HomePageComponent, 
             FooterComponent, LoginComponent, RestablecerContrasenaComponent, 
-            Registro01Component, ReactiveFormsModule,DashBoardComponent
-          ],
+            Registro01Component, ReactiveFormsModule,DashBoardComponent],
   templateUrl: './app.component.html',
- // styleUrl: './app.component.css'
- styleUrls: ['assets/css/style.css'] 
-
+  styleUrls: ['assets/css/style.css'], 
   imports: [
     RouterOutlet,
     NadvarComponent,
@@ -43,11 +33,9 @@ import { CommonModule } from '@angular/common'; // Importar CommonModule
     LoginComponent,
     RestablecerContrasenaComponent,
     CommonModule // Agregar CommonModule aquí
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Corregir a styleUrls
- develop
+  ]
 })
+  
 export class AppComponent {
   title = 'appBio';
 }
