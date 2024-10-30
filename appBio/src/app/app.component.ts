87d9729155953f7e89d61ddab32b2ba6e6ab8b89
Paz <109832507+Paz-Santangelo@users.noreply.github.com>
2024-10-30 15:16:13 -0300

    Corrigiendo nuevamente errores de app.component.ts

--- appBio/src/app/app.component.ts
+++ appBio/src/app/app.component.ts
@@ -1,39 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Registro01Component } from './registro01/registro01/registro01.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
 import { NadvarComponent } from './shared/nadvar/nadvar.component';
 import { HomePageComponent } from './shared/home-page/home-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { RegistroComponent } from "./registro/registro.component";
 import { AceptarTerminosComponent } from "./aceptar-terminos/aceptar-terminos.component";
 import { RestablecerContrasenaComponent } from './restablecer-contrasena/restablecer-contrasena.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashBoardComponent } from './Dash/dash-board/dash-board.component';
-import { QuienessomosComponent } from "./QuienesSomos/quienessomos/quienessomos.component";
 import { LoginComponent } from "./login/login.component";
-import { CommonModule } from '@angular/common'; // Importar CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NadvarComponent, HomePageComponent, 
             FooterComponent, LoginComponent, RestablecerContrasenaComponent, 
-            Registro01Component, ReactiveFormsModule,DashBoardComponent],
+            RegistroComponent, ReactiveFormsModule, DashBoardComponent, AceptarTerminosComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['assets/css/style.css'], 
-  imports: [
-    RouterOutlet,
-    NadvarComponent,
-    HomePageComponent,
-    FooterComponent,
-    RegistroComponent,
-    AceptarTerminosComponent,
-    QuienessomosComponent,
-    LoginComponent,
-    RestablecerContrasenaComponent,
-    CommonModule // Agregar CommonModule aquí
-  ]
+  styleUrls: ['assets/css/style.css']
 })
   
 export class AppComponent {
