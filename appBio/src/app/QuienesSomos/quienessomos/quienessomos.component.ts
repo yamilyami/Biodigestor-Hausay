38018e4162469a95b817c20ef34b163f77d9bf0e
Paz-Santangelo <paz_santangelo@hotmail.com>
2024-10-30 18:55:29 -0300

    Restauracion de carpeta quienesSomos por conflictos con develop

---
+++ appBio/src/app/QuienesSomos/quienessomos/quienessomos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-quienessomos',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './quienessomos.component.html',
+  styleUrl: './quienessomos.component.css'
+})
+export class QuienessomosComponent {
+
+}
